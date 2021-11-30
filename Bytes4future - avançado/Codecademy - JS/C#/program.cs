77232c995781db32f1044da3caa9f811be03bac6
Caweca105 <lemos.diogo105@gmail.com>
2021-11-30 16:20:31 +0000

    Create program.cs

---
+++ Bytes4future - avançado/Codecademy - JS/C#/program.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      //The objective is to make a ghost
+      Console.WriteLine("     (    )        ");   
+      Console.WriteLine("  (  (   )  )      ");   
+      Console.WriteLine("(  (       )  )    ");   
+      Console.WriteLine("  (  (   )    )    ");   
+      Console.WriteLine("     (     )       ");   
+      Console.WriteLine("    /--------\\    ");    
+      Console.WriteLine("   /   \\   /  \\  ");
+      Console.WriteLine("  /     0 0    \\  ");
+      Console.WriteLine(" /       o      \\ ");
+      Console.WriteLine(" ****************  ");
+      Console.WriteLine("   Angry Volcano   ");
+     }
+  }
+}
