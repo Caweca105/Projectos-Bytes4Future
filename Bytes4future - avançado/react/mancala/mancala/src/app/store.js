6c997e1537cd2329af6c3a441d7a91439df36c59
Caweca105 <lemos.diogo105@gmail.com>
2021-12-02 00:34:43 +0000

    Deleted some useless files

--- Bytes4future - avançado/react/mancala/mancala/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import mancalaReducer from "./Mancala/mancalaSlice";
-
-export default configureStore({
-    reducer: { 
-        mancala: mancalaReducer
-    }
-})
